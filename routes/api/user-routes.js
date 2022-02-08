c1e3ada4fccfa6ed6bd2f838599864173eff2ef6
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-02-07 22:04:24 -0800

    api frame work

---
+++ routes/api/user-routes.js
@@ -0,0 +1,99 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET  api users
+router.get('/', (req, res) => {
+// access user model .findAll method = to SELECT * FROM users;
+User.findAll({ 
+    attributes: { exclude: ['password']}
+})
+.then(dbUserData => res.json(dbUserData))
+.catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+// GET  api users/1
+router.get('/:id', (req, res) => {
+    // Equal to SELECT * FROM users WHERE id = 1
+    User.findOne({ 
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// POST  api users
+router.post('/login', (req, res) => {
+    // expects {username: 'USERNAME', email: 'name@gmail.com', password: 'passcode12'} equal to  INSERT INTO users /VALUES
+  (username, email, password)
+  VALUES
+    ("Lernantino", "lernantino@gmail.com", "password1234");
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+// PUT  api users/1
+router.put('/:id', (req, res) => {  User.update(req.body, {
+
+    //UPDATE users SET username = "'USERNAME', email= 'name@gmail.com', password= newpasscode12'"
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE api users/1
+router.delete('/:id', (req, res) => {  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 8800;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(routes);
+
+// Turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
